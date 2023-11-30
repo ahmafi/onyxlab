e07de97073a9555f0f984f3ddb71d54fe816b82a
Amirhossein Mafi <amir77mafi@gmail.com>
2023-11-30 23:07:31 +0330

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,9 @@ const vazirmatn = Vazirmatn({
   variable: "--persian-font",
 });
 
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  applicationName: "OnyxLab",
+};
 
 export const viewport: Viewport = {
   themeColor: [
@@ -33,7 +35,30 @@ export default function RootLayout({
       dir="rtl"
       className={clsx("w-full h-full", vazirmatn.variable, inter.variable)}
     >
-      <GoogleAnalytics />
+      <head>
+        <GoogleAnalytics />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d232a" />
+        <meta name="apple-mobile-web-app-title" content="OnyxLab" />
+        <meta name="msapplication-TileColor" content="#1d232a" />
+      </head>
       <body className="w-full h-full font-persian flex flex-col">
         <Header />
         {children}
